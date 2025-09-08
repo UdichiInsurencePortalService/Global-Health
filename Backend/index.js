78e774598a5e96fa3e732dae8700d2364eec7dfb
Diamour <diamourweb@gmail.com>
2025-09-08 11:49:34 +0530

    new changes

--- Backend/index.js
+++ Backend/index.js
@@ -268,6 +268,6 @@ app.get("/logout", (req, res) => {
 });
 
 // ✅ Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is running on http://0.0.0.0:${PORT}`);
 });
